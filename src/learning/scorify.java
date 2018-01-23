6e6411d66c6ef1c11755d29aceb12c070bfa57b3
Youssef <Youssef@UNKNOWN>
2018-01-23 10:12:27 +0000

    add "" to system out

--- src/learning/scorify.java
+++ src/learning/scorify.java
@@ -12,23 +12,22 @@ public class scorify {
         Scanner sc = new Scanner(new File("STDIN"));
 
         int numberOfTests = sc.nextInt();
-        
+
         for (int i = 0; i < numberOfTests; i++) {
             int d = sc.nextInt();
-            int [] res = new int [5];
-            
-            
+            int[] res = new int[5];
+
             for (int j = 0; j < 5; j++) {
                 res[j] = sc.nextInt() * d;
             }
             BigInteger re = new BigInteger("0");
             for (int j = 0; j < 5; j++) {
                 BigInteger pow = new BigInteger(sc.next());
-                re = re.add(new BigInteger(res[j]+"").modPow(pow, new BigInteger("50")));
+                re = re.add(new BigInteger(res[j] + "").modPow(pow, new BigInteger("50")));
             }
-            
-            System.out.println(re.mod(new BigInteger("50")));
-            
+
+            System.out.println(re.mod(new BigInteger("50")) + "All of you");
+
         }
     }
 }
